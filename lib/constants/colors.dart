3cb9f887b728419192236ebedc61e3fe5d7eb480
Sefa Çalık <sefacalij1212@gmail.com>
2025-06-10 22:37:07 +0300

    AppBar Modification and Body colorization

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBlue = Color(0xFF1976D2);
+  static const Color primaryLight = Color(0xFF63A4FF);
+  static const Color primaryDark = Color(0xFF004BA0);
+  static const Color textWhite = Colors.white;
+  static const Color accentBlue = Color(0xFF82B1FF);
+  static const Color textPrimary = Color(0xFF212121);
+  static const Color textSecondary = Color(0xFF757575);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
+// ignore_for_file: camel_case_types
+
 import 'package:flutter/material.dart';
-import 'package:water_reminding_project/utils/colors.dart';
-import 'package:water_reminding_project/utils/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:water_reminding_project/constants/colors.dart';
 
 void main() {
-  runApp(water_tracker_program());
+  runApp(water_tracker_app());
 }
 
-class water_tracker_program extends StatelessWidget {
-  const water_tracker_program({super.key});
+class water_tracker_app extends StatelessWidget {
+  const water_tracker_app({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Su Takip Uygulaması',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePage(), // Uygulama açılınca gösterilecek ekran
-      debugShowCheckedModeBanner: false, // Debug yazısını gizle
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const HomePage(),
     );
   }
 }
@@ -30,6 +31,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings, color: AppColors.textWhite),
+          ),
+        ],
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: AppColors.textWhite),
+          onPressed: () {},
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 31, 80, 164),
+        title: Text(
+          "Welcome Babe",
+          style: GoogleFonts.quicksand(
+            color: AppColors.textWhite,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Container(color: AppColors.accentBlue),
+    );
   }
 }
